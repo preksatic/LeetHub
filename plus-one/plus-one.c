d2185c1127571e971df32f72f921f9a2efe163eb
jsmaster777 <79179032+jsmaster777@users.noreply.github.com>
2022-11-13 16:44:09 +0900

    Time: 0 ms, Memory: 6 MB - LeetHub

--- plus-one/plus-one.c
+++ plus-one/plus-one.c
@@ -1,26 +1,23 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
-
-int* plusOne(int* digits, int digitsSize, int* returnSize) { 
-    *returnSize=digitsSize+1;
-    while(digitsSize!=0)
-    {
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+    *returnSize = digitsSize + 1;
+    while (digitsSize != 0){
         digitsSize--;
-        if(++digits[digitsSize]>9)
-        {digits[digitsSize]=0;}
-        else
-        {
-            (*returnSize)--;
+        if (++digits[digitsSize] > 9){
+            digits[digitsSize] = 0;
+        }
+        else{
+            --*returnSize;
             return digits;
         }
     }
-    int *result=(int*)malloc(sizeof(int)*(*returnSize));
-    result[0]=1;
-    for(int i=1;i<*returnSize;i++)
-    {
-        result[i]=digits[i-1];
+    int *a = (int*)malloc(sizeof(int)*(*returnSize));
+    a[0]= 1;
+    for (int i = 1; i < *returnSize; i++){
+        a[i] = 0;
     }
-    return result;
+    return a;
 }
+
